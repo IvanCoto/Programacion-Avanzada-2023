14590b9fa13fc4e477f38c1a8b3a3c442cb9d36d
WaltZX230 <87626613+WaltZX230@users.noreply.github.com>
2023-04-04 22:57:04 -0600

    Avances de Producto
    Está casi todo, solo falta la migración

---
+++ Inventario.API/Controllers/ProductoController.cs
@@ -0,0 +1,72 @@
+﻿using Inventario.Application.Contracts.Services;
+using Inventario.Domain.EntityModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventario.API.Controllers
+{
+    public class ProductoController : ControllerBase
+    {
+
+
+        public ProductoController(IProductoService service)
+        {
+            _service = service;
+        }
+        readonly IProductoService _service;
+
+        [HttpGet]
+        [ActionName(nameof(List))]
+        public ActionResult<IEnumerable<Producto>> List()
+        {
+            return _service.List().ToList();
+        }
+
+        [HttpGet("{id}")]
+        [ActionName(nameof(Get))]
+        public ActionResult<Producto> Get(int id)
+        {
+            return _service.Get(id);
+        }
+
+        [HttpPost]
+        [ActionName(nameof(Post))]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public ActionResult Post(Producto producto)
+        {
+            _service.Insert(producto);
+            return CreatedAtAction(nameof(Get), new { id = producto.Id }, producto);
+        }
+
+        [HttpPut]
+        [ActionName(nameof(Put))]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult Put(Producto producto)
+        {
+            try
+            { _service.Update(producto); }
+            catch (DbUpdateConcurrencyException)
+            { return NotFound(); }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ActionName(nameof(Delete))]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult Delete(int id)
+        {
+            try
+            { _service.Delete(id); }
+            catch (ArgumentNullException)
+            { return NotFound(); }
+
+            return NoContent();
+        }
+    }
+}
---
+++ Inventario.Application/Contracts/Services/IProductoService.cs
@@ -0,0 +1,20 @@
+﻿using Inventario.Domain.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario.Application.Contracts.Services
+{
+    public interface IProductoService
+    {
+        Producto Get(int id);
+        IEnumerable<Producto> List(Expression<Func<Producto, bool>> predicate = null);
+        void Insert(Producto producto);
+        void Update(Producto producto);
+        void Delete(int id);
+        void Delete(Producto producto);
+    }
+}
